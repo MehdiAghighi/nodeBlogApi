daf6ceaa689e35f52c087022bc02f3de46f9f7ef
root <root@127.0.0.1>
2020-02-13 02:23:54 -0500

    Adding Routes, Creating Post, Fetch All Posts And Fetch Single Posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,19 +1,74 @@
+const { validationResult } = require('express-validator');
+
+// Models
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [ { title: "Post #1", content: "this is some post content" } ]
-    });
+    Post.find()
+        .then(posts => {
+            return res.status(200)
+                .json({
+                    message: "Successfuly Fetched Posts.",
+                    posts: posts
+                })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
 }
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation Fialed. User Inputs Are Incorrect')
+        error.statusCode = 422;
+        throw error;
+    }
     const title = req.body.title;
     const content = req.body.content;
-    // Create In DB
-    res.status(201).json({
-        message: "Post Created Succesfuly",
-        post: {
-            id: new Date().toISOString(),
-            title: title,
-            content: content
-        }
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: "images/duck.jpg",
+        creator: {
+            name: "Max"
+        },
     })
+    post.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: "Post Created Succesfuly",
+                post: result
+            })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Post Not Found');
+                error.statusCode = 404;
+                throw error;
+            }
+            return res.status(200).json({ message: "Post Fetched", post: post });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
 }
--- nodeBlogApi.js
+++ nodeBlogApi.js
@@ -1,8 +1,11 @@
 // Node Packages
+const path = require('path');
 
 // 3rd Party Packages
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Controllers
 
@@ -12,7 +15,31 @@ const feedRoutes = require('./routes/feed');
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use( (err, req, res, next) => {
+    console.log(err);
+    const status = err.statusCode || 500;
+    const message = err.message;
+    res.status(status)
+        .json({
+            message: message,
+        })
+})
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(res => {
+        app.listen(8080);
+    })
+    .catch(err => {
+        console.log(err);
+    })
