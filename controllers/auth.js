a369ddc5d534fcdf26fccfed62cf102da20bacdf
root <root@127.0.0.1>
2020-03-07 09:33:49 -0500

    added authentication with jsonWebTokens, signing up, logging in and getting the token ( Not Authorizing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -26,6 +27,45 @@ exports.signup = ( req, res, next ) => {
         })
         .then(result => {
             res.status(201).json({ message: "User Created !", userId: result._id })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+}
+
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let foundUser;
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                const err = new Error('User With this email not found');
+                err.statusCode = 401;
+                throw err;
+            }
+            foundUser = user
+            return bcrypt.compare(password, user.password);
+        })
+        .then(isEqual => {
+            if (!isEqual) {
+                const err = new Error("Wrong password");
+                err.statusCode = 401;
+                throw err;
+            }
+            const token = jwt.sign({
+                email: email,
+                useId: foundUser._id.toString()
+            },
+            process.env.JWT_SECRET, 
+            { expiresIn: '1h' })
+            res.status(200).json({
+                token: token,
+                userId: foundUser._id.toString()
+            })
         })
         .catch(err => {
             if (!err.statusCode) {
